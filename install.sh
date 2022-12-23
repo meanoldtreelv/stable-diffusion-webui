de4a4d821b161ecc13d9d54d74758bcb9cf9137c
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-12-23 01:26:33 -0600

    split install to account for conda reload

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@ apt install python3.10 pip
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 chmod +x Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 bash Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
-~/miniconda3/bin/conda create --prefix a1111 python=3.10.6
-~/miniconda3/bin/conda activate $PWD/a1111
-git init .
-git remote add origin https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
-git pull origin master
-pip install -r requirements.txt
-pip install triton
-~/miniconda3/bin/conda install xformers -c xformers/label/dev
-wget https://github.com/meanoldtreelv/stable-diffusion-webui/blob/7dc1219f4163443606e6c88824139c390de8dc77/conda-ui.sh
+wget https://github.com/meanoldtreelv/stable-diffusion-webui/blob/c9d9a02207f6a53525595edf601d1516ed121f9b/install2.sh
+chmod +x install2.sh
+bash install2.sh
