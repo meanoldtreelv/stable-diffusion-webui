3e6cf4fda5abc6daa6f31a6cb35c4768e736d2c9
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-12-23 01:32:36 -0600

    reload bash with exec

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ sudo apt install python3.10 pip
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 chmod +x Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 bash Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
-wget https://raw.githubusercontent.com/meanoldtreelv/stable-diffusion-webui/master/install2.sh
-chmod +x install2.sh
-bash install2.sh
+exec bash
+~/miniconda3/bin/conda create --prefix a1111 python=3.10.6
+~/miniconda3/bin/conda activate $PWD/a1111
+git init .
+git remote add origin https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+git pull origin master
+pip install -r requirements.txt
+pip install triton
+~/miniconda3/bin/conda install xformers -c xformers/label/dev
+wget https://raw.githubusercontent.com/meanoldtreelv/stable-diffusion-webui/master/conda-ui.sh
